3e7728a82be11ecb6127efaf4d3b9613e589eddc
KABAGEMA <kabagemabenj@gmail.com>
2024-12-01 06:48:50 -0800

    added icons toggle js

--- src/components/TopMenuBar.js
+++ src/components/TopMenuBar.js
@@ -3,17 +3,45 @@ import { FaBars, FaTimes, FaSearch, FaBell, FaHeart, FaCartArrowDown } from 'rea
 import './TopMenuBar.css';
 
 const TopMenuBar = () => {
+        /*  humburger toggle */
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+        /*  cart toggle */
+
   const [isCartOpen, setIsCartOpen] = useState(false);
 
     const toggleCart = () => {
       setIsCartOpen(!isCartOpen);
     };
+
+     /*  search toggle */
+
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+   /*  Notification toggle */
+
+   const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+
+   const toggleNotification = () => {
+     setIsNotificationOpen(!isNotificationOpen);
+   };
+
+    /*  Favorite toggle */
+
+  const [isFavoriteOpen, setIsFavoriteOpen] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavoriteOpen(!isFavoriteOpen);
+  };
 
   return (
     <div className="top-menu-bar">
@@ -39,15 +67,28 @@ const TopMenuBar = () => {
       </nav>
         <div className="blankSpace"></div>
       <div className="icons">
-      <FaSearch className="menu-icon" />
-      <FaBell className="menu-icon" />
-      <FaHeart className="menu-icon" />
-      <FaCartArrowDown className="menu-icon" onMouseEnter={toggleCart} />
+      <FaSearch className="menu-icon" onClick={toggleSearch} />
+      <FaBell className="menu-icon" onClick={toggleNotification}/>
+      <FaHeart className="menu-icon"  onClick={toggleFavorite}/>
+      <FaCartArrowDown className="menu-icon" onClick={toggleCart} />
       </div>
 
       <div className={`cart ${isCartOpen ? 'cart-open' : ''}`}>
         cart
       </div>
+
+      <div className={`notification ${isNotificationOpen ? 'notification-open' : ''}`}>
+        Notification
+      </div>
+
+      <div className={`search ${isSearchOpen ? 'search-open' : ''}`}>
+        search
+      </div>
+
+      <div className={`favorite ${isFavoriteOpen ? 'favorite-open' : ''}`}>
+        Favorite
+      </div>
+
     </div>
 
   );
