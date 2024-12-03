d910293df7d4a7fb8293e323d0374b8aae430b73
KABAGEMA <kabagemabenj@gmail.com>
2024-12-03 07:44:41 -0800

    created for everyone page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 import ToggleList from './components/TopMenuBar';
 import Parallax  from './components/body';
+import ShopCategories from './components/forEveryone';
+
 function App() {
   return (
     <div>
       
       <ToggleList/>
       <Parallax/>
+      <ShopCategories/>
       
     </div>
   );
---
+++ src/components/forEveryone.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles/forEveryone.css';
+
+const ShopCategories = () => {
+  const categories = [
+    {
+        title: 'Shop / Men',
+      image: `${process.env.PUBLIC_URL}/images/4.jpg`,
+    },
+    {
+      title: 'Shop / Accessories',
+      image: `${process.env.PUBLIC_URL}/images/5.jpg`,
+    },
+    {
+      title: 'Shop / Women',
+      image: `${process.env.PUBLIC_URL}/images/6.jpg`,
+    },
+    {
+      title: 'Shop / Kids',
+      image: `${process.env.PUBLIC_URL}/images/7.jpg`,
+    },
+  ];
+
+  return (
+    <div className="shop-container">
+      <h2>SOMETHING FOR EVERYONE</h2>
+      <div className="shop-grid">
+        {categories.map((category, index) => (
+          <div className="shop-item" key={index}>
+            <img src={category.image} alt={category.title} />
+            
+            <p>{category.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategories;
